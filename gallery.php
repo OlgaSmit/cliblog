34a6b7088f15de8864e0069fe69a245f4efe1a5a
OlgaSmit <olganeon2@gmail.com>
2025-04-03 22:28:31 +0300

    commit3

---
+++ gallery.php
@@ -0,0 +1,5 @@
+<?php
+//как в браузере появляется html css js
+//SPA react angulat vue js --> index.html client -> api -> backend ajax
+
+//SSR html -> backend +SEO (будем генерировать страницы полностью)
